4a82e79b5e29e87d1c077a5d0837fa4b71b5729a
kevinmainairungu <mainairungu99@gmail.com>
2020-04-22 16:02:28 +0300

    adding clicklisteners to the text once clicked to change the background color

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setlisteners()
+    }
+
+    private fun setlisteners() {
+        val clickableviews : List<View> =
+            listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text)
+        for (item in clickableviews) {
+            item.setOnClickListener{makeColored(it)}
+        }
+    }
+
+    private fun makeColored(it: View?) {
+        when (View.id) {
+//            boxes using color class color for background
+            R.id.box_one_text -> it?.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> it?.setBackgroundColor(Color.GRAY)
+        }
+
     }
 }
